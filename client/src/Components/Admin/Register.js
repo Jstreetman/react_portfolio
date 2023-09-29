4245e3a547ec5537304251efb8c05e4bae71cb37
Jonathan Streetman <jonathanstreetman@Jonathans-MacBook-Air-2.local>
2023-09-28 21:23:49 -0400

    Added Some Admin Components

---
+++ client/src/Components/Admin/Register.js
@@ -0,0 +1,11 @@
+import Navbar from "../Navbar";
+import "bootstrap/dist/css/bootstrap.css";
+
+function Register() {
+  return (
+    <section className="">
+      <Navbar />
+    </section>
+  );
+}
+export default Register;
--- client/src/index.js
+++ client/src/index.js
@@ -1,12 +1,35 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import { ThemeProvider } from "@mui/material/styles";
 import theme from "./Components/Themes/Themes";
+import Register from "./Components/Admin/Register";
+
+const router = createBrowserRouter([
+  {
+    path: "/admin",
+    element: <Register />,
+  },
+
+  {
+    path: "/adminpanel",
+    element: "",
+  },
+
+  {
+    path: "/login",
+    element: "",
+  },
+  {
+    path: "/",
+    element: <App />,
+  },
+]);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <ThemeProvider theme={theme}>
-    <App />
+    <RouterProvider router={router} />
   </ThemeProvider>
 );
